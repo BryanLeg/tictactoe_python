46a400dec41c256d0d7270e9d28375b61f468590
Bryan Legrain <brylegrain@gmail.com>
2023-05-31 17:05:29 +0200

    feat: initalisation of the project

---
+++ tictactoe.py
@@ -0,0 +1,10 @@
+import random
+
+class TicTacToe:
+    """A class to represent the game
+    """
+
+    def __init__(self):
+        """Initalize a clear board
+        """
+        self.board = []
